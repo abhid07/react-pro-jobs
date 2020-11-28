3a6805d2c9c834da4ee768e71c1a30a8052372d5
abhid07 <a.deshpande4509@gmail.com>
2020-11-28 17:14:15 +0530

    FSJ0820B104

---
+++ projobs/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import HomeComponent from './Components/HomeComponent';
+
+
+function App() {
+
+  return (
+    <div>
+      <HomeComponent/>
+    </div>
+  );
+}
+
+export default App;
